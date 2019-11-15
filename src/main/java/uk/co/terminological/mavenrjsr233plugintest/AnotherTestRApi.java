fc29cb4f09ccceb8df333574ecb93874a89f25a5
Rob Challen <rob@terminological.co.uk>
2019-11-15 15:46:10 +0000

    update

---
+++ src/main/java/uk/co/terminological/mavenrjsr233plugintest/AnotherTestRApi.java
@@ -0,0 +1,45 @@
+package uk.co.terminological.mavenrjsr233plugintest;
+
+import uk.co.terminological.jsr223.RClass;
+import uk.co.terminological.jsr223.RMethod;
+
+
+/**
+ * This has no documentation
+ */
+@RClass
+public class AnotherTestRApi {
+
+	String message1;
+	String message2;
+	
+	/**
+	 * the first constructor is used if there are none annotated 
+	 * @param message1 - the message to be printed 
+	 * @param message2 - will be used for toString
+	 */
+	public AnotherTestRApi(String message1, String message2) {
+		this.message1 = message1;
+		this.message2 = message2;
+	}
+	
+	@RMethod
+	public String throwCatchable() throws Exception {
+		throw new Exception("A catchable exception has been thrown");
+	}
+	
+	@RMethod
+	public void printMessage() {
+		System.out.println("Printed in java: "+message1);
+	}
+	
+	@RMethod
+	public String throwRuntime() {
+		throw new RuntimeException("A runtime exception has been thrown");
+	}
+	
+	public String toString() {
+		return "toString: "+message2;
+	}
+	
+}
--- src/main/java/uk/co/terminological/mavenrjsr233plugintest/TestRApi.java
+++ src/main/java/uk/co/terminological/mavenrjsr233plugintest/TestRApi.java
@@ -3,18 +3,27 @@ package uk.co.terminological.mavenrjsr233plugintest;
 import uk.co.terminological.jsr223.RClass;
 import uk.co.terminological.jsr223.RMethod;
 
+
+/**
+ * A test of the jsr223 templating
+ * 
+ * this is a details comment 
+ * @author given family email@example.com ORCIDID
+ * 
+ */
 @RClass(
-		licence = "MIT", 
-		packageName = "apitest", 
-		title = "Some function",
-		version = "0.1"
+		imports = {"ggplot2","dplyr"},
+		suggests = {"roxygen2","devtools"}
 		)
 public class TestRApi {
 
 	String message;
 	
-	@RMethod(title = "Shows a hello world message",
-			examples = {
+	/**
+	 * Description of a hello world function
+	 * @return this java method returns a String
+	 */
+	@RMethod(examples = {
 					"An example",
 					"Spans many lines"
 			})
@@ -22,18 +31,33 @@ public class TestRApi {
 		return "Hello world from Java!";
 	}
 	
-	@RMethod(title = "sets a message")
+	/**
+	 * A fluent method description
+	 * @param message the message is a string
+	 * @return nothing as this is a fluent method
+	 */
+	@RMethod
 	public TestRApi fluentSetMessage(String message) {
 		this.message = message;
 		return this;
 	}
 	
-	@RMethod(title = "gets the message")
+	/**
+	 * message desciption
+	 * @return A message previously set or maybe null
+	 */
+	@RMethod
 	public String getMessage() {
 		return message;
 	}
 	
-	@RMethod(title = "does a sum")
+	/**
+	 * The doSum function description
+	 * @param a the A parameter
+	 * @param b the B parameter
+	 * @return A+B of course
+	 */
+	@RMethod
 	public Double doSum(Double a, Double b) {
 		return a+b;
 	}
