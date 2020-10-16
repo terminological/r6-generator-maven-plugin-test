65250fb6dc25c26e15b16392d5bd84f1bf3d5dbe
Rob Challen <rob@terminological.co.uk>
2020-10-16 17:12:08 +0100

    name update

---
+++ commitAll.sh
@@ -0,0 +1,13 @@
+ 
+cd ~/Git/r6-generator-runtime
+git add -A
+git commit -m "$1"
+git push
+cd ~/Git/r6-generator-maven-plugin
+git add -A
+git commit -m "$1"
+git push
+cd ~/Git/r6-generator-maven-plugin-test
+git add -A
+git commit -m "$1"
+git push
---
+++ installAll.sh
@@ -0,0 +1,6 @@
+cd ~/Git/r6-generator-runtime
+mvn install
+cd ~/Git/r6-generator-maven-plugin
+mvn install
+cd ~/Git/r6-generator-maven-plugin-test
+mvn install
